5e35abdcbfde4e4b666d78df65771b4cc848ada2
theDruid <arnaud.ferre.pro@gmail.com>
2014-08-24 16:26:35 +0200

    Try to debug my GIT config...

--- SmellGame/settings.py
+++ SmellGame/settings.py
@@ -14,7 +14,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 print BASE_DIR
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
 
