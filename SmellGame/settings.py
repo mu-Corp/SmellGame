634005d7123c5995c99ffc51d686b926c3981b5a
Jean Coquet <coquet.jean@gmail.com>
2014-10-13 15:14:32 +0200

    iframe permission

--- SmellGame/settings.py
+++ SmellGame/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     #'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    #'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 ROOT_URLCONF = 'SmellGame.urls'
