69f63082855bbb545f8eb661ef32512cbe89d3e6
Nurtal <nathan.foulquier.pro@gmail.com>
2014-10-23 21:51:29 +0200

    Highchart graphics (include js librairy)
    Page Graphique fonctionelle en "StandAlone", création d'une vue et d'une
    url mais pas encore pu tester (soucis de serveur). + Importation de la
    librairie Javascript d'hightChart (pas complète, juste les fichiers qui
    servent)

--- SmellGalaxy/urls.py
+++ SmellGalaxy/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('SmellGalaxy.views',
                        #url(r'^URL_name/$', 'AppName.views.FunctionName'),
+					   url(r'^$', 'Graphes'), #Demo highchart
 )
--- SmellGalaxy/views.py
+++ SmellGalaxy/views.py
@@ -1,4 +1,11 @@
 ﻿#-*- coding: utf-8 -*-
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def DemoGraphes(request):
+	"""
+	A simple view for a simple demonstration
+	"""
+	return render(request, 'SmellGalaxyTemplate/Graphes.html')
