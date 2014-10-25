5e52ed7af21eb5064f155131f9fd15beba073e7b
JeanCoq <pro@jeancoquet.com>
2014-10-25 16:54:47 +0200

    Progress of app Galaxy

--- SmellGalaxy/models.py
+++ SmellGalaxy/models.py
@@ -1,4 +1,216 @@
 ﻿#-*- coding: utf-8 -*-
-from django.db import models
 
-# Create your models here.
+################################################################
+#######################    LIBRARIES    ########################
+################################################################
+
+from django.db import models
+from collections import defaultdict
+import random
+
+from SmellGuess.models   import *
+from SmellGift.models    import *
+
+################################################################
+#######################    FUNCTIONS    ########################
+################################################################
+#Plus utils
+def getSmeller(givers): 
+	i = 0
+	for eachGiver in givers : 
+		#print "Giver Infos :", eachGiver.id
+		GuessByIdSample = Guess.objects.filter(sample_id=eachGiver.id)
+		moyIntensity = doMoyenneIntensity(GuessByIdSample)
+		moyFeeling   = doMoyenneFeeling(GuessByIdSample)
+		i += len(GuessByIdSample)
+		#print "Moyenne intensité - ", doMoyenneIntensity(GuessByIdSample)
+		#print "Moyenne feeling   - ", doMoyenneFeeling(GuessByIdSample)
+		#print "\n--------------"
+	print "total ech : ", i
+
+#todo doMoyForAllGivers():
+def doMoyFeelingForAllGivers (givers) :
+	moyenne = 0; somme = 0
+	i=0
+	for eachGiver in givers : 
+		GuessByIdSample = Guess.objects.filter(sample_id=eachGiver.id)
+		if len(GuessByIdSample) != 0 :
+			for elt in GuessByIdSample :
+				somme += elt.feeling
+				i     += 1
+	moyenne = (somme* 1.0)/ i
+	moyenne = round(moyenne,2)
+	return moyenne
+
+def doMoyIntensityForAllGivers (givers) :
+	moyenne = 0; somme = 0
+	i=0
+	for eachGiver in givers : 
+		GuessByIdSample = Guess.objects.filter(sample_id=eachGiver.id)
+		if len(GuessByIdSample) != 0 :
+			for elt in GuessByIdSample :
+				somme += elt.intensity
+				i     += 1
+	moyenne = (somme* 1.0)/ i
+	moyenne = round(moyenne,2)
+	return moyenne
+
+
+def getDataByDeo():
+	giverWithDeo    = SampleGiver.objects.filter(deodorant = 1)
+	giverWithoutDeo = SampleGiver.objects.filter(deodorant = 0)
+	result = [['People who use deo - intensity',doMoyIntensityForAllGivers(giverWithDeo)],['People who use deo - feeling',doMoyFeelingForAllGivers(giverWithDeo)],['People who not use deo - intensity',doMoyIntensityForAllGivers(giverWithoutDeo)],['People who not use deo - feeling',doMoyFeelingForAllGivers(giverWithoutDeo)]]
+	return result
+
+def getDataBySex():
+	giverWoman   = SampleGiver.objects.filter(sex = "F")
+	giverMan = SampleGiver.objects.filter(sex = "M")
+	result = [['Man people - intensity',doMoyIntensityForAllGivers(giverMan)],['Man people - feeling',doMoyFeelingForAllGivers(giverMan)],['Woman people - intensity',doMoyIntensityForAllGivers(giverWoman)],['Woman people - feeling',doMoyFeelingForAllGivers(giverWoman)]]
+	return result
+
+def getDataBySliceOfAge(): #todo
+	dict_SliceOfAge = {}
+	result = ()
+	
+	dict_SliceOfAge["giver0_15"]   = SampleGiver.objects.extra( select = {'ofSlice': "age > '0' & age < 16"})
+	dict_SliceOfAge["giver15_30"]  = SampleGiver.objects.extra( select = {'ofSlice': "age > '15' & age < 31"})
+	dict_SliceOfAge["giver30_60"]  = SampleGiver.objects.extra( select = {'ofSlice': "age > '30' & age < 61"})
+	dict_SliceOfAge["giver60more"] = SampleGiver.objects.extra( select = {'ofSlice': "age > '60'"})
+
+	result = [
+		["giver0_15_Intensity",   doMoyIntensityForAllGivers( dict_SliceOfAge["giver0_15"])],
+		['giver0_15_feeling',     doMoyFeelingForAllGivers(   dict_SliceOfAge["giver0_15"])],
+		["giver15_30_Intensity",  doMoyIntensityForAllGivers( dict_SliceOfAge["giver15_30"])],
+		['giver15_30_feeling',    doMoyFeelingForAllGivers(   dict_SliceOfAge["giver15_30"])],
+		["giver30_60_Intensity",  doMoyIntensityForAllGivers( dict_SliceOfAge["giver30_60"])],
+		['giver30_60_feeling',    doMoyFeelingForAllGivers(   dict_SliceOfAge["giver30_60"])],
+		["giver60more_Intensity", doMoyIntensityForAllGivers( dict_SliceOfAge["giver60more"])],
+		['giver60more_feeling',   doMoyFeelingForAllGivers(   dict_SliceOfAge["giver60more"])] ]
+	
+	return result
+
+def intensityByFeeling():
+	allGuess = Guess.objects.all()
+	dictData= defaultdict(lambda:list())
+	string = []
+	for eachGuess in allGuess :
+		dictData[eachGuess.intensity].append((int(eachGuess.feeling)))
+	for eachKey in dictData.keys() : 
+		string.append([str(eachKey), moyList(dictData[eachKey])])
+	return string
+	
+def intensityByFeelingCenter():
+	allGuess = Guess.objects.all()
+	dictData= defaultdict(lambda:list())
+	string = []
+	for eachGuess in allGuess :
+		dictData[eachGuess.intensity].append((int(eachGuess.feeling)-50))
+	for eachKey in dictData.keys() : 
+		string.append([str(eachKey), moyList(dictData[eachKey])])
+	return string
+	
+def moyList(aList):
+	moyenne = 0.0; somme = 0.0
+	if len(aList) > 0 : 
+		for elt in aList : 
+			somme += elt
+		moyenne = somme / len(aList)
+	moyenne = round(moyenne,2)
+	return moyenne
+
+
+def pieByCritereSmoker():
+	allGiver    = SampleGiver.objects.all()
+	giverTrue   = SampleGiver.objects.filter(smoker = True)
+	giverFalse  = SampleGiver.objects.filter(smoker = False)
+	giverOther  = SampleGiver.objects.filter(smoker__isnull=True)
+	
+	ratioTrue  = getRatio(giverTrue, allGiver)
+	ratioFalse = getRatio(giverFalse, allGiver)
+	ratioOther = getRatio(giverOther, allGiver)
+	stringToReturn = [['Smoker',ratioTrue],['No smoker',ratioFalse],['empty field',ratioOther]]
+	return stringToReturn
+
+def getRatio(Sub, All, dataNull = ()):
+	ratio = 1.0 * len(Sub)  / (len(All)-len(dataNull))
+	ratio = round(ratio,2)
+	return ratio
+
+def pieByCritereSex():
+	allGiver   = SampleGiver.objects.all()
+	giverWoman = SampleGiver.objects.filter(sex = "F")
+	giverMan   = SampleGiver.objects.filter(sex = "M")
+	giverOther = SampleGiver.objects.filter(sex__isnull=True)
+	
+	ratioWoman = getRatio(giverWoman, allGiver, giverOther)
+	ratioMan   = getRatio(giverMan,   allGiver, giverOther)
+	stringToReturn = [['Woman',ratioWoman],['Man',ratioMan]]
+	return stringToReturn
+	
+def pieByCritereSliceOfAge():
+	#todo fix value of select
+	allGiver    = SampleGiver.objects.all()
+	giver0_15   = SampleGiver.objects.extra( select = {'ofSlice': "age > '0' & age < 16"})
+	giver15_30  = SampleGiver.objects.extra( select = {'ofSlice': "age > '15' & age < 31"})
+	giver30_60  = SampleGiver.objects.extra( select = {'ofSlice': "age > '30' & age < 61"})
+	giver60more = SampleGiver.objects.extra( select = {'ofSlice': "age > '60'"})
+	giverOther  = SampleGiver.objects.filter(age__isnull=True)
+	
+	ratio0_15   = getRatio(giver0_15,   allGiver, giverOther)
+	ratio15_30  = getRatio(giver15_30,  allGiver, giverOther)
+	ratio30_60  = getRatio(giver30_60,  allGiver, giverOther)
+	ratio60more = getRatio(giver60more, allGiver, giverOther)
+	stringToReturn = [['ratio0_15',ratio0_15],['ratio15_30',ratio15_30],['ratio30_60',ratio30_60],['ratio60more',ratio60more]]
+	return stringToReturn
+	
+################################################################
+########################    CLASSES    #########################
+################################################################
+
+class GraphHistogramme(models.Model):
+	id           = models.AutoField(primary_key=True)
+	title        = models.CharField(max_length=8096)
+	button       = models.CharField(max_length=8096)
+	orderedName  = models.CharField(max_length=256)
+	abscisseName = models.CharField(max_length=256)
+	data         = models.CharField(max_length=8096)
+	dataError    = models.CharField(max_length=8096)
+	#graphHisto = GraphHistogramme(title="", button="", orderedName="", abscisseName="", data=getdata...(), dataError =getdata...())
+
+
+class GraphPie(models.Model):
+	id          = models.AutoField(primary_key=True)
+	title       = models.CharField(max_length=512)
+	button      = models.CharField(max_length=512)
+	description = models.CharField(max_length=512)
+	data        = models.CharField(max_length=8096)
+	#graph= GraphPie(title="", button="", description="", data=getdata...())
+
+class GraphLines(models.Model):
+	id          = models.AutoField(primary_key=True)
+	title          = models.CharField(max_length=320)
+	subtitle       = models.CharField(max_length=320)
+	button         = models.CharField(max_length=320)
+	abscisseName   = models.CharField(max_length=256)
+	orderedName    = models.CharField(max_length=256)
+	nameLine1  = models.CharField(max_length=256)
+	dataLine1  = models.CharField(max_length=8096)
+	nameLine2  = models.CharField(max_length=256)
+	dataLine2  = models.CharField(max_length=8096)
+	#graphLines = GraphLines(title="", button="", abscisseName="", orderedName="", nameLine1="",dataLine1=getdata...(), nameLine2="", dataLine2 =getdata...())
+		
+###############################################################
+####################    LOCAL EXECUTION    ####################
+###############################################################
+if __name__ == "__main__" :
+
+	print 'Test in local\n.'
+
+	print 'Test By Deo\n'
+	print getDataByDeo()
+	
+	print 'Test in Sex\n'
+	print getDataBySex()
+	
+	print 'Test in Sex\n'
+	print getDataBySliceOfAge()
--- SmellGalaxy/views.py
+++ SmellGalaxy/views.py
@@ -1,11 +1,81 @@
 ﻿#-*- coding: utf-8 -*-
-from django.shortcuts import render
-from django.http import HttpResponse
+
+"""
+Authors : 
+- Florian Thonier, florian.thonier@gmail.com (2014)
+"""
 
 
-# Create your views here.
+################################################################
+#######################    LIBRARIES    ########################
+################################################################
+# Django libs:
+from django.http import HttpResponse, Http404
+from django.shortcuts import redirect, render
+from django.core.servers.basehttp import FileWrapper
+from django.utils.encoding import smart_str, smart_unicode 
+
+# External libs:
+from datetime import datetime
+import random
+import time
+import codecs
+import os, tempfile, zipfile
+from base64 import decodestring
+
+from django.db import models
+from SmellGuess.models  import *
+from SmellGalaxy.models import *
+from SmellGift.models   import *
+
+################################################################
+#########################    VIEWS    ##########################
+################################################################
+
+
+# Nathan view
 def graphesView(request):
-	"""
-	A simple view for a simple demonstration
-	"""
-	return render(request, 'SmellGalaxyTemplate/Graphes.html')
+    paramToGenerateTemplate = dict()
+
+    paramToGenerateTemplate['guess']             = Guess.objects.all()
+    paramToGenerateTemplate['sample']            = Sample.objects.all()
+	#Delete old data
+    GraphHistogramme.objects.all().delete()
+    GraphPie.objects.all().delete()
+    GraphLines.objects.all().delete()
+    #Get news data
+    paramToGenerateTemplate['HistoGraphs']  = createListHisto()
+    paramToGenerateTemplate['PiesGraphs']   = createListPie()
+    paramToGenerateTemplate['LinesGraphs']  = createListLines()
+
+    return render(request, 'SmellGalaxyTemplate/Graphes.html', paramToGenerateTemplate) 
+    
+def createListHisto():
+	#GraphHistogramme(title="", button="", orderedName="", abscisseName="", data=getdata...(), dataError =getdata...())
+	if not GraphHistogramme.objects.filter(title="Intensity & Feeling by sex"): 
+		GraphHistogramme(title="Intensity & Feeling by sex", button="Data by sex", orderedName="Intensity & feeling", abscisseName="sex of giver", data= getDataBySex(), dataError = "").save()
+	if not GraphHistogramme.objects.filter(title="Intensity & Feeling by age"): 
+		GraphHistogramme(title="Intensity & Feeling by age", button="Data by age", orderedName="Intensity & feeling", abscisseName="categories of age", data= getDataBySliceOfAge(), dataError = "").save()
+	if not GraphHistogramme.objects.filter(title="Intensity & Feeling by use of deodorant"): 
+		GraphHistogramme(title="Intensity & Feeling by use of deodorant", button="Data by use of deodorant", orderedName="Intensity & feeling", abscisseName="use of deodorant of giver", data= getDataByDeo(), dataError = "").save()
+	toReturn = GraphHistogramme.objects.all()
+	return toReturn
+
+
+def createListPie():
+	#GraphPie(title="", button="", description="", data=getdata...())
+	if not GraphPie.objects.filter(title="Repartition by sex"): 	
+		GraphPie(title="Repartition by sex",               button="Pie by sex",     description="Repartition by sex", data=pieByCritereSex).save()
+	if not GraphPie.objects.filter(title="Repartition between slice of Age"): 	
+		GraphPie(title="Repartition between slice of Age", button="Pie by age",     description="Repartition between slice of Age", data=pieByCritereSliceOfAge()).save()
+	if not GraphPie.objects.filter(title="Repartition by smoker"): 	
+		GraphPie(title="Repartition by smoker",            button="Pie by smoking", description="Repartition by smoker", data=pieByCritereSmoker()).save()
+	toReturn = GraphPie.objects.all()
+	return toReturn
+
+def createListLines():
+	#GraphLines(title="", button="", abscisseName="", orderedName="", nameLine1="",dataLine1=getdata...(), nameLine2="", dataLine2 =getdata...())
+	if not GraphLines.objects.filter(title="Pleasanteness in function of intensity"): 	
+		GraphLines(title="Pleasanteness in function of intensity", button="pleasanteness by intensity", abscisseName="Intensity", orderedName="Feeling", nameLine1="raw data", dataLine1=intensityByFeeling(), nameLine2="Center data", dataLine2 =intensityByFeelingCenter(), subtitle="").save()
+	toReturn = GraphLines.objects.all()
+	return toReturn
